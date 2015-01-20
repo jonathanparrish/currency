4cd510b6e4da94098b13e37932a6b8beb111b8e4
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-20 18:49:06 -0500

    Add test 02 set equal  and code to currency file

--- currency.rb
+++ currency.rb
@@ -1,9 +1,17 @@
 class Currency
-  attr_reader :amount
+  attr_reader :amount, :code
 
-  def initialize(amount)
+  def initialize(amount, code)
     @amount = amount
+    @code = code
+  end
 
+  def ==(currency)
+    if currency.amount == @amount && currency.code == @code
+      return true
+    else
+      return false
+    end
   end
 
 end
--- currency_test.rb
+++ currency_test.rb
@@ -28,8 +28,22 @@ class CurrencyTest< Minitest::Test
     assert Currency
   end
   #Should be created with an amount and a currency code
-  def test_01_currency_knows_its_ammount
-      currency = Currency.new(23)
-      assert_equal 23, currency.amount
+  def test_01_currency_knows_its_ammount_and_currency_code
+    currency = Currency.new(23, "EUR")
+    assert_equal 23, currency.amount
+    assert_equal "EUR", currency.code
   end
+
+  def test_02_currency_equals_object_with_same_amt_and_code
+    currency = Currency.new(23, "EUR")
+    currency_2= Currency.new(23, "EUR")
+    assert currency == currency_2
+  end
+
+
+
+
+
+
+
 end
