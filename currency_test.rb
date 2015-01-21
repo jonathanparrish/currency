587578660bf312042bbe961a477bc7add43d9159
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-20 19:02:49 -0500

    Add test 03 able to add currency with same code

--- currency_test.rb
+++ currency_test.rb
@@ -29,19 +29,23 @@ class CurrencyTest< Minitest::Test
   end
   #Should be created with an amount and a currency code
   def test_01_currency_knows_its_ammount_and_currency_code
-    currency = Currency.new(23, "EUR")
-    assert_equal 23, currency.amount
+    currency = Currency.new(50, "EUR")
+    assert_equal 50, currency.amount
     assert_equal "EUR", currency.code
   end
 
   def test_02_currency_equals_object_with_same_amt_and_code
-    currency = Currency.new(23, "EUR")
-    currency_2= Currency.new(23, "EUR")
+    currency = Currency.new(50, "EUR")
+    currency_2= Currency.new(50, "EUR")
     assert currency == currency_2
   end
 
+  def test_03_able_to_add_to_another_currency_obj_with_same_code
+    low_amount = Currency.new(50, "EUR")
+    high_amount = Currency.new(100, "EUR")
+    assert_equal 150, low_amount + high_amount
 
-
+  end
 
 
 
